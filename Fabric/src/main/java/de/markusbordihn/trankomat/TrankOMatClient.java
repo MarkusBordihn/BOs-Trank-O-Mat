755e0390785efcdcef22b7042df70a98376fc59e
Markus Bordihn <Markus@Bordihn.de>
2023-11-30 21:23:01 +0100

    Improved Fabric server compatibility and fixed some smaller issues.

--- Fabric/src/main/java/de/markusbordihn/trankomat/TrankOMatClient.java
+++ Fabric/src/main/java/de/markusbordihn/trankomat/TrankOMatClient.java
@@ -21,33 +21,34 @@ package de.markusbordihn.trankomat;
 
 import de.markusbordihn.trankomat.block.SodaDepositMachineBlocks;
 import de.markusbordihn.trankomat.block.SodaVendingMachineBlocks;
-import de.markusbordihn.trankomat.blocks.ModBlocks;
-import de.markusbordihn.trankomat.item.ModItems;
-import de.markusbordihn.trankomat.sounds.ModSoundEvents;
-import de.markusbordihn.trankomat.tabs.ModTabs;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.minecraft.client.renderer.RenderType;
 
-public class TrankOMat implements ModInitializer {
+public class TrankOMatClient implements ClientModInitializer {
 
   @Override
-  public void onInitialize() {
+  public void onInitializeClient() {
     // Use Fabric to bootstrap the Common mod.
-    Constants.LOG.info("Initializing {} (Fabric) ...", Constants.MOD_NAME);
+    Constants.LOG.info("Initializing {} (Fabric-Client) ...", Constants.MOD_NAME);
     CommonClass.init();
 
-    Constants.LOG.info("{} Blocks ...", Constants.LOG_REGISTER_PREFIX);
-    ModBlocks.registerModBlocks();
+    Constants.LOG.info("{} Register Block Layers ...", Constants.LOG_REGISTER_PREFIX);
+    BlockRenderLayerMap.INSTANCE.putBlock(
+        SodaDepositMachineBlocks.DEPOSIT_MACHINE_TOP, RenderType.translucent());
+    BlockRenderLayerMap.INSTANCE.putBlock(
+        SodaDepositMachineBlocks.SODA_DEPOSIT_MACHINE_01, RenderType.cutoutMipped());
 
-    Constants.LOG.info("{} Items ...", Constants.LOG_REGISTER_PREFIX);
-    ModItems.registerModItems();
+    BlockRenderLayerMap.INSTANCE.putBlock(
+        SodaVendingMachineBlocks.VENDING_MACHINE_TOP, RenderType.translucent());
+    BlockRenderLayerMap.INSTANCE.putBlock(
+        SodaVendingMachineBlocks.SODA_VENDING_MACHINE_01, RenderType.cutoutMipped());
+    BlockRenderLayerMap.INSTANCE.putBlock(
+        SodaVendingMachineBlocks.SODA_VENDING_MACHINE_02, RenderType.cutoutMipped());
 
-    Constants.LOG.info("{} Item Groups ...", Constants.LOG_REGISTER_PREFIX);
-    ModTabs.registerModTabs();
-
-    Constants.LOG.info("{} Sound Events ...", Constants.LOG_REGISTER_PREFIX);
-    ModSoundEvents.registerModSoundEvents();
+    // Some code like events require special initialization from the
+    // loader specific code.
+    ItemTooltipCallback.EVENT.register(CommonClass::onItemTooltip);
   }
 }
